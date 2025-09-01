f4b2d600092351c00a8f8b074229399da5fca40c
m.m.baderik <m.m.baderik@tbank.ru>
2025-09-01 16:07:32 +1000

    m: added tests for table functions

--- test/table_functions/test_read_edges.cpp
+++ test/table_functions/test_read_edges.cpp
@@ -2,135 +2,152 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
 
-#include "duckdb_graphar_extension.hpp"
+#include <filesystem>  
+#include <iostream>  
+
+#include "test_table_functions.hpp"
 #include "functions/table/read_edges.hpp"
-
-#include <duckdb/function/table_function.hpp>
-#include <duckdb/main/client_context.hpp>
-#include <duckdb/main/database.hpp>
-#include <duckdb/common/types.hpp>
-#include <duckdb/common/vector.hpp>
-
-#include <graphar/graph_info.h>
-// #include <graphar/edge_info.h>
 
 using namespace duckdb;
 using namespace graphar;
 
-// Вспомогательная функция для создания тестового контекста
-unique_ptr<ClientContext> CreateMockContext() {
-    return ClientContext::Create();
-}
-
-// Вспомогательная функция для создания тестового GraphInfo
-std::shared_ptr<GraphInfo> CreateTestGraphInfo() {
-    // В реальности это должен быть полноценный GraphInfo с файлом
-    auto graph_info = std::make_shared<GraphInfo>();
-    // Здесь должен быть код для создания тестового графа
-    return graph_info;
-}
-
-TEST_CASE("ReadEdges Bind function basic test", "[read_edges]") {
-    auto context = CreateMockContext();
-    TableFunctionBindInput input;
-    vector<LogicalType> return_types;
-    vector<string> names;
-
-    // Подготовка входных данных
-    input.inputs.push_back(Value("test.graphar"));
-    input.named_parameters["src"] = Value("Person");
-    input.named_parameters["dst"] = Value("City");
-    input.named_parameters["type"] = Value("LivesIn");
-
-    auto bind_data = ReadEdges::Bind(*context, input, return_types, names);
-
-    REQUIRE(bind_data != nullptr);
-    // Добавьте дополнительные проверки в соответствии с ожидаемым поведением
-}
-
-TEST_CASE("ReadEdges Bind function invalid_edge", "[read_edges]") {
-    auto context = CreateMockContext();
-    TableFunctionBindInput input;
-    vector<LogicalType> return_types;
-    vector<string> names;
-
-    // Подготовка входных данных
-    input.inputs.push_back(Value("test.graphar"));
-    input.named_parameters["src"] = Value("Invalid");
-    input.named_parameters["dst"] = Value("City");
-    input.named_parameters["type"] = Value("LivesIn");
-
-    // Ожидаем исключение, так как такого ребра нет
-    REQUIRE_THROWS_AS(ReadEdges::Bind(*context, input, return_types, names), BinderException);
-}
-
-TEST_CASE("ReadEdges GetReader test", "[read_edges]") {
-    // Подготовка тестовых данных
-    auto graph_info = CreateTestGraphInfo();
-    auto edge_info = graph_info->GetEdgeInfo("Person", "LivesIn", "City");
-    
-    unique_ptr<ReadBindData> bind_data = make_uniq<ReadBindData>();
-    ReadEdges::SetBindData(graph_info, *edge_info, bind_data);
-
-    ReadBaseGlobalTableFunctionState gstate;
-    
-    // Тестирование GetReader
-    auto reader = ReadEdges::GetReader(gstate, *bind_data, 0, "", "", "");
-    REQUIRE(reader != nullptr);
-    
-    // Тестирование с фильтром по src
-    reader = ReadEdges::GetReader(gstate, *bind_data, 0, "123", SRC_GID_COLUMN, "int64");
-    REQUIRE(reader != nullptr);
-    
-    // Тестирование с фильтром по dst
-    reader = ReadEdges::GetReader(gstate, *bind_data, 0, "456", DST_GID_COLUMN, "int64");
-    REQUIRE(reader != nullptr);
-}
-
-TEST_CASE("ReadEdges SetFilter test", "[read_edges]") {
-    // Подготовка тестовых данных
-    auto graph_info = CreateTestGraphInfo();
-    auto edge_info = graph_info->GetEdgeInfo("Person", "LivesIn", "City");
-    
-    unique_ptr<ReadBindData> bind_data = make_uniq<ReadBindData>();
-    ReadEdges::SetBindData(graph_info, *edge_info, bind_data);
-
-    ReadBaseGlobalTableFunctionState gstate;
-    
-    std::string filter_value = "123";
-    std::string filter_column = SRC_GID_COLUMN;
-    std::string filter_type = "int64";
-    
-    // Тестирование установки фильтра
-    ReadEdges::SetFilter(gstate, *bind_data, filter_value, filter_column, filter_type);
-    
-    // Проверка, что фильтр установлен
-    REQUIRE(gstate.filter_range.first == 0);
-    // Может потребовать дополнительных проверок в зависимости от реализации
-}
-
-TEST_CASE("ReadEdges GetFunction test", "[read_edges]") {
+TEST_CASE("ReadEdges GetFunction basic test", "[read_edges]") {
     TableFunction read_edges = ReadEdges::GetFunction();
     
-    // Проверка базовых свойств функции
     REQUIRE(read_edges.name == "read_edges");
-    REQUIRE(read_edges.parameters.size() == 1);
+    REQUIRE(read_edges.arguments.size() == 1);
     REQUIRE(read_edges.named_parameters.size() == 3);
     REQUIRE(read_edges.filter_pushdown == true);
     REQUIRE(read_edges.projection_pushdown == true);
     
-    // Проверка наличия всех необходимных параметров
     REQUIRE(read_edges.named_parameters.find("src") != read_edges.named_parameters.end());
     REQUIRE(read_edges.named_parameters.find("dst") != read_edges.named_parameters.end());
     REQUIRE(read_edges.named_parameters.find("type") != read_edges.named_parameters.end());
 }
 
-TEST_CASE("ReadEdges GetScanFunction test", "[read_edges]") {
+TEST_CASE("ReadEdges Bind function basic test", "[read_edges]") {
+    TableFunction read_edges = ReadEdges::GetFunction();
+
+    INFO("Start mocking");
+    DuckDB db(nullptr); 
+    Connection conn(db);
+    
+    vector<Value> inputs({Value(GRAPH_YML_PATH)});
+    named_parameter_map_t named_parameters({{"src", Value("Person")}, {"dst", Value("Person")}, {"type", Value("knows")}});
+    vector<LogicalType> input_table_types({});
+    auto input = СreateMockBindInput(inputs, named_parameters, input_table_types);    
+    
+    vector<LogicalType> return_types;
+    vector<std::string> names;
+    INFO("Finish mocking");
+
+    
+    INFO("Start bind");
+    
+    auto bind_data = read_edges.bind(*conn.context, input, return_types, names);
+
+    INFO("Finish bind");
+
+    REQUIRE(bind_data != nullptr);
+    REQUIRE(return_types == vector<LogicalType> ({LogicalType::BIGINT, LogicalType::BIGINT}));
+    REQUIRE(names == vector<std::string> ({SRC_GID_COLUMN, DST_GID_COLUMN}));
+}
+
+TEST_CASE("ReadEdges Bind function invalid_edge", "[read_edges]") {
+    TableFunction read_edges = ReadEdges::GetFunction();
+
+    INFO("Start mocking");
+    DuckDB db(nullptr); 
+    Connection conn(db);
+    
+    vector<Value> inputs({Value(GRAPH_YML_PATH)});
+    named_parameter_map_t named_parameters({{"src", Value("Person")}, {"dst", Value("Person")}, {"type", Value("invalid_edge")}});
+    vector<LogicalType> input_table_types({});
+    auto input = СreateMockBindInput(inputs, named_parameters, input_table_types);    
+        
+    vector<LogicalType> return_types;
+    vector<std::string> names;
+    INFO("Finish mocking");
+
+    REQUIRE_THROWS_AS(read_edges.bind(*conn.context, input, return_types, names), BinderException);
+}
+
+TEST_CASE("ReadEdges Bind function edge with property", "[read_edges]") {
+    TableFunction read_edges = ReadEdges::GetFunction();
+    INFO("Start mocking");
+    DuckDB db(nullptr); 
+    Connection conn(db);
+    
+    vector<Value> inputs({Value(LDBC_GRAPH_YML_PATH)});
+    named_parameter_map_t named_parameters({{"src", Value("person")}, {"dst", Value("person")}, {"type", Value("knows")}});
+    vector<LogicalType> input_table_types({});
+    auto input = СreateMockBindInput(inputs, named_parameters, input_table_types);    
+    
+    vector<LogicalType> return_types;
+    vector<std::string> names;
+    INFO("Finish mocking");
+
+    INFO("Start bind");
+    
+    auto bind_data = read_edges.bind(*conn.context, input, return_types, names);
+
+    INFO("Finish bind");
+
+    REQUIRE(bind_data != nullptr);
+    REQUIRE(return_types == vector<LogicalType> ({LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::VARCHAR}));
+    REQUIRE(names == vector<std::string> ({SRC_GID_COLUMN, DST_GID_COLUMN, "creationDate"}));
+}
+
+
+TEST_CASE("ReadEdges GetScanFunction basic test", "[read_edges]") {
     TableFunction scan_edges = ReadEdges::GetScanFunction();
     
-    // Проверка базовых свойств сканирующей функции
     REQUIRE(scan_edges.name == "");
-    REQUIRE(scan_edges.parameters.empty());
+    REQUIRE(scan_edges.arguments.empty());
     REQUIRE(scan_edges.filter_pushdown == true);
     REQUIRE(scan_edges.projection_pushdown == true);
+}
+
+
+TEST_CASE("ReadEdges GetReader test", "[read_edges]") {
+    INFO("Creating db");
+    DuckDB db(nullptr);
+    Connection conn(db);
+
+    INFO("Start mocking");
+
+    vector<Value> inputs({Value(GRAPH_YML_PATH)});
+    named_parameter_map_t named_parameters({{"src", Value("Person")}, {"dst", Value("Person")}, {"type", Value("knows")}});
+    vector<LogicalType> input_table_types;
+    auto input = СreateMockBindInput(inputs, named_parameters, input_table_types);
+
+    vector<LogicalType> return_types;
+    vector<std::string> names;
+    INFO("Finish mocking");
+
+    auto bind_data_uniq = ReadEdges::GetFunction().bind(*conn.context, input, return_types, names);
+    auto& bind_data = bind_data_uniq->Cast<ReadBindData>();
+
+    ReadBaseGlobalTableFunctionState gstate;
+
+    SECTION("Тест без фильтра"){
+        auto reader = ReadEdges::GetReader(gstate, bind_data, 0, "", "", "");
+        REQUIRE(reader != nullptr);
+
+        auto result = GetChunk(*reader);
+        REQUIRE(!result.has_error());
+        auto table = result.value();
+        REQUIRE(table->num_rows() == 578006);
+        REQUIRE(table->num_columns() == 2);
+    }
+
+    SECTION("Тест с фильтром по SRC"){
+        auto reader = ReadEdges::GetReader(gstate, bind_data, 0, "0", SRC_GID_COLUMN, "int64");
+        REQUIRE(reader != nullptr);
+
+        auto result = GetChunk(*reader);
+        REQUIRE(!result.has_error());
+        auto table = result.value();
+        REQUIRE(table->num_rows() == 1); // только одно ребро выходящее из вершины 0
+    }
 }
---
+++ test/table_functions/test_table_functions.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "duckdb_graphar_extension.hpp"
+#include "functions/table/read_vertices.hpp"
+
+#include <duckdb/function/table_function.hpp>
+#include <duckdb/main/client_context.hpp>
+#include <duckdb/main/database.hpp>
+#include <duckdb/common/types.hpp>
+#include <duckdb/common/vector.hpp>
+#include "duckdb/parser/tableref/table_function_ref.hpp"
+
+#include <graphar/graph_info.h>
+
+using namespace duckdb;
+using namespace graphar;
+
+inline std::string TEST_DIR = "/Users/m.m.baderik/Projects/duckdb-graphar-mbaderik/test/data/";
+inline std::string GRAPH_YML_PATH = TEST_DIR + "git/Git.yaml";
+inline std::string LDBC_GRAPH_YML_PATH = "/Users/m.m.baderik/Projects/duckdb-graphar-mbaderik/build/_deps/graphar-prefix/src/graphar/testing/ldbc_sample/parquet/ldbc_sample.graph.yml";
+inline std::string LDBC_GRAPH_WITH_FEATURE_YML_PATH = "/Users/m.m.baderik/Projects/duckdb-graphar-mbaderik/build/_deps/graphar-prefix/src/graphar/testing/ldbc_sample/parquet/ldbc_sample_with_feature.graph.yml";
+
+TableFunctionBindInput СreateMockBindInput(vector<Value> &inputs, named_parameter_map_t &named_parameters, vector<LogicalType> &input_table_types) {
+    vector<std::string> input_table_names;
+    TableFunction table_function;
+    TableFunctionRef ref;
+
+    return TableFunctionBindInput(inputs, named_parameters, input_table_types,
+                                  input_table_names, nullptr, nullptr,
+                                  table_function, ref);
+}
